007cfb633cfd07e63bc758f62fe0d00125ffdae2
ChrisTree <cuicuitrees@gmail.com>
2018-06-02 13:05:47 +0800

    commit

--- src/infinityLoveBox.java
+++ src/infinityLoveBox.java
@@ -3,22 +3,24 @@ import java.awt.*;
 
 public class infinityLoveBox {
     private static void boxGUI() {
+        //set frame layout
         JFrame.setDefaultLookAndFeelDecorated(true);
         JFrame frame = new JFrame("infinityLove");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new GridLayout(5,2));
 
 
-
+        //add label
         JLabel jLabel = new JLabel("Are you ZhangYan?");
         frame.add(jLabel);
 
 
-
+        //creat yes and no button
         JButton yesButton = new JButton("yes");
         JButton noButton = new JButton("no");
 
 
+        //add yesbutton and yesbutton_actionlistener
         frame.add(yesButton);
         yesButton.addActionListener(e -> {
             int resbonse1 = JOptionPane.showOptionDialog(null, "Do you know Cui Zigang?", "infinityLove",
@@ -30,6 +32,7 @@ public class infinityLoveBox {
                         null, null, null);
             }
 
+            //click yesbutton in the first dialog
             if (resbonse1 == JOptionPane.YES_OPTION) {
                 int resbonse2 = JOptionPane.showOptionDialog(null, "Ohh, do you love him?", "infinityLove",
                         JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE,
@@ -40,6 +43,7 @@ public class infinityLoveBox {
                             null, null, null);
                 }
 
+                //click yesbutton in the second dialog
                 if (resbonse2 == JOptionPane.YES_OPTION) {
                     int resbonse3 = JOptionPane.showOptionDialog(null, "Then is the love deep?", "infinityLove",
                             JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE,
@@ -50,21 +54,59 @@ public class infinityLoveBox {
                                 null, null, null);
                     }
 
+                    //click yesbutton in the third dialog
                     if (resbonse3 == JOptionPane.YES_OPTION) {
-                        JOptionPane.showOptionDialog(null, "You are my lover\n" +
-                                        "You have to eat every meal during a day\n" +
-                                        "You have to sleep earlier\n" +
-                                        "You can not eat spicy\n" +
-                                        "Do not eat cold during the period\n" +
-                                        "I love you forever\n" +
-                                        "FUCK THE DIRT WORLD!!!", "LOVE YOU FOREVER♥♥♥♥",
-                                JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE,
+                        int resbonse4 = JOptionPane.showOptionDialog(null, "Do you want him to find you?", "infinityLove",
+                                JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE,
                                 null, null, null);
+
+                        if (resbonse4 == JOptionPane.NO_OPTION) {
+                            JOptionPane.showOptionDialog(null, "please click yes", "tips",
+                                    JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE,
+                                    null, null, null);
+                        }
+
+                        //click yesbutton in the fourth dialog
+                        if (resbonse4 == JOptionPane.YES_OPTION) {
+                            int resbonse5 = JOptionPane.showOptionDialog(null, "Can I find you the next national day?", "infinityLove",
+                                    JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE,
+                                    null, null, null);
+
+                            if (resbonse5 == JOptionPane.NO_OPTION) {
+                                JOptionPane.showOptionDialog(null, "please click yes", "tips",
+                                        JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE,
+                                        null, null, null);
+                            }
+
+                            //click yesbutton in the fifth dialog
+                            if (resbonse5 == JOptionPane.YES_OPTION) {
+                                JOptionPane.showOptionDialog(null, "You are my lover\n" +
+                                                "You are the one that I won't share\n" +
+                                                "Wherever I go\n" +
+                                                "whoever I know" +
+                                                "However the times flow" +
+                                                "You are one piece of my heart\n" +
+                                                "You are the one I don't wanna lose\n" +
+                                                "So baby you must listen to me\n" +
+                                                "For example:\n" +
+                                                "Don't be thinner\n"+
+                                                "You have to eat every meal during a day\n" +
+                                                "You have to sleep earlier\n" +
+                                                "You can not eat spicy\n" +
+                                                "Do not eat cold just like ice cream during the period\n" +
+                                                "I love you forever\n" +
+                                                "FUCK THE DIRTY WORLD!!!", "LOVE YOU FOREVER♥♥♥♥",
+                                        JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE,
+                                        null, null, null);
+                            }
+                        }
+
                     }
                 }
             }
         });
 
+        //add nobutton and nobutton_actionlistener
         frame.add(noButton);
         noButton.addActionListener(e -> JOptionPane.showOptionDialog(null, "please click yes", "tips",
                 JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE,
@@ -77,19 +119,29 @@ public class infinityLoveBox {
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
     public static void main(String[] args) {
         SwingUtilities.invokeLater(infinityLoveBox::boxGUI);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
