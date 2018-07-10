3e6c9e9552e2e391c8491c08b749c612d3bfddff
ChrisTree <cuicuitrees@gmail.com>
2018-07-10 23:05:23 +0800

    commit

--- src/selection.java
+++ src/selection.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class selection {
     protected int response;
 
-    public void yesSelection(String string1 , String string2) {
+    public void yesSelection(String string1) {
         response = JOptionPane.showOptionDialog(null, string1, "infinityLove",
                 JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE,
                 null, null, null);
@@ -12,8 +12,9 @@ public class selection {
                     JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE,
                     null, null, null);
         }
+        //出现死循环了，下次使用设计模式解决！工厂模式或者抽象工厂模式。2018年7月10号更新，尝试优化infinityLoveBox失败，无法给yesSelection方法给与两个参数。
         if (response == JOptionPane.YES_OPTION) {
-            yesSelection()
+            yesSelection(string1);
         }
     }
 }
